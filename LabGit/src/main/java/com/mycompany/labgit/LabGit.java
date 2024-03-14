452fa47bed047b17a944ce5fe75bf76a0e004ef0
anmutya <mutovkina.ania@gmail.com>
2024-03-14 11:05:56 +0300

    CreateLabGit
    ляляял

---
+++ LabGit/src/main/java/com/mycompany/labgit/LabGit.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.labgit;
+
+/**
+ *
+ * @author annamutovkina
+ */
+public class LabGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int a;
+        a = -1;
+    }
+}
